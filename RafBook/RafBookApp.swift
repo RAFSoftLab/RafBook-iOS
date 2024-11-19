248688f51f4ac81f92e8fc9b1ac4fee811930eb4
Stevan Dabizljevic <stevandabizljevic@Stevans-MacBook-Pro.local>
2024-11-19 19:02:08 +0100

    initials project commit

---
+++ RafBook/RafBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafBookApp.swift
+//  RafBook
+//
+//  Created by Stevan Dabizljevic on 19.11.24..
+//
+
+import SwiftUI
+
+@main
+struct RafBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RafBookTests/RafBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  RafBookTests.swift
+//  RafBookTests
+//
+//  Created by Stevan Dabizljevic on 19.11.24..
+//
+
+import Testing
+@testable import RafBook
+
+struct RafBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
